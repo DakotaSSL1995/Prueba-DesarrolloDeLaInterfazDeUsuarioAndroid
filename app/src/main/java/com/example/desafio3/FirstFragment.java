3963e01ae321a829cf3399028b99961f30f39931
DakotaSSL1995 <danielaspp1995@gmail.com>
2024-06-01 20:56:52 -0400

    Prueba control de versiones

---
+++ app/src/main/java/com/example/desafio3/FirstFragment.java
@@ -0,0 +1,67 @@
+package com.example.desafio3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import androidx.annotation.Nullable;
+
+import com.example.desafio3.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setEnabled(false);
+
+        binding.passwordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                validatePassword(s.toString());
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+
+        binding.buttonFirst.setOnClickListener(v ->
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void validatePassword(String password) {
+        boolean isValid = password.length() > 5 && password.matches(".*[A-Z].*");
+        binding.buttonFirst.setEnabled(isValid);
+    }
+}
